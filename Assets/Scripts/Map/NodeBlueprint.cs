ad7b5e1743e9baf72989bb9332518a4f8ee2465b
Chloe Law <chloelaw75@hotmail.com>
2022-12-03 17:02:06 -0500

    Initial map commit

---
+++ Assets/Scripts/Map/NodeBlueprint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Map
+{
+	public enum NodeType
+	{
+		MinorEnemy,
+		EliteEnemy,
+		RestSite,
+		RandomEvent,
+		Boss
+	}
+}
+
+namespace Map
+{
+	[CreateAssetMenu]
+	public class NodeBlueprint : ScriptableObject
+	{
+		public Sprite sprite;
+		public NodeType nodeType;
+	}
+}
